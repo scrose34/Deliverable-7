94b1557d85c843dbb4791647ea1188bde2b9c12e
Kleeborp <Kleeborp@DESKTOP-05QGDPB>
2021-03-08 21:38:04 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deliverable_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            webBrowser1.ObjectForScripting = this;
+            webBrowser1.ScriptErrorsSuppressed = false;
+
+            webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            webBrowser1.AllowWebBrowserDrop = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string CurrentDirectory = CurrentDirectory.GetCurrentDirectory();
+            webBrowser1.Navigate(Path.Combine(CurrentDirectory, "HTMLPageForJAvaScript.html"));
+
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webBrowser1.Focus();
+            Report();
+
+        }
+        public void PrintReport()
+        {
+            DialogResult dr = printDialog1.ShowDialog();
+            if (dr.ToString() == "OK")
+            {
+                webBrowser1.Print();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
